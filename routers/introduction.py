53fcc58fd2c8ab1fdc1e98c543104af60670c809
Ashar Farooq <ashar744@gmail.com>
2025-02-26 21:41:14 -0500

    introduction

--- routers/introduction.py
+++ routers/introduction.py
@@ -10,17 +10,17 @@ def introduce(intro: Introduction):
     introductions.append(intro)
 
     return {
-        "message": f"{intro.consumer} has been introduced to {intro.operator} via {intro.service}",
+        "message": f"{intro.operator} has been introduced to {intro.service} to track data for {intro.consumer}",
         "introduction": intro
     }
 
-@router.get("/introduced/{consumer}/{operator}")
+@router.get("/introduced")
 def introduced(consumer: str, operator: str):
     """
     Checks if an operator is connected to any service on behalf of a consumer.
     """
     for intro in introductions:
         if intro.consumer == consumer and intro.operator == operator:
-            return {"consumer": consumer, "operator": operator, "service": intro.service}
+            return {"service": intro.service}
 
     raise HTTPException(status_code=404, detail="No introduction found")
